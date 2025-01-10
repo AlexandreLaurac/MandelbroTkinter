724d4b81ecaaca07745acf8b486d358ec8c277f1
AlexandreLaurac <alexandre_laurac@yahoo.fr>
2025-01-10 14:29:18 +0100

    Création d'une classe pour l'ensemble de Mandelbrot

--- ensemble_Mandelbrot.py
+++ ensemble_Mandelbrot.py
@@ -1,80 +1,120 @@
-# -*- coding: utf-8 -*-
-
-
 from tkinter import *
 from math import sqrt
 
-########################### Définition des fonctions ##########################
 
-def trace_ensemble (minx, maxx, miny, maxy) :
-    "Fonction traçant l'ensemble de Mandelbrot pour la région de dimension donnée en argument"
-    # Paramètres
-    Nit = 100                 # Nombre d'itérations pour le test d'appartenance
-    Kx = (maxx-minx)/largeur  # pente en x pour la transition pixel -> abscisse
-    Ky = (maxy-miny)/hauteur  # pente en y pour la transition pixel -> ordonnée
-    # Parcours des pixels pour la zone choisie
-    for px in range(largeur) :
-        x = Kx * px + minx
-        for py in range(hauteur) :
-            # Coordonnées du point courant
-            y = Ky * py + miny
-            # 1er test (le point appartient à la cardioïde ou au bourgeon principal) 
-            p = sqrt ((x-0.25)**2 + y**2)
-            if (x < p-2*p**2+0.25) or ((x+1)**2+y**2 < 1./16) :
-                borne = 1
-            # 2è test
-            else :
-                c = complex (x,y)
-                # Calcul des Nit+1 termes de la suite
-                z = complex(0,0)
-                borne = 1
-                for nit in range(1,Nit+1) :
-                    z = z*z + c
-                    if abs(z) > 2 :
-                        borne = 0
-                        break
-            # Tracé
-            if borne :
+#---------------------------------------- Modèle ----------------------------------------#
+
+class Mandelbrot():
+    """Classe modélisant l'ensemble de Mandelbrot pour une zone du plan donnée
+    et un nombre de points en abscisse et en ordonnée sur cette zone
+    """
+
+    def __init__(self, tuple_nb_points, tuple_zone_init, n_iter=100):
+        self.nb_points_x = tuple_nb_points[0]
+        self.nb_points_y = tuple_nb_points[1]
+        self.K = self.nb_points_y / self.nb_points_x  # rapport des dimensions, pour les pixels ET pour la zone de représentation
+        self.init_zone_repr(tuple_zone_init)
+        self.init_ensemble()
+        self.n_iter = n_iter
+
+    def init_zone_repr(self, tuple_zone_repr):
+        self.x_min = tuple_zone_repr[0]
+        self.x_max = tuple_zone_repr[1]
+        self.Kxy = (self.x_max - self.x_min) / self.nb_points_x  # Kxy == Kx == Ky
+        self.y_min = tuple_zone_repr[2]
+        self.y_max = self.K * (self.x_max - self.x_min) + self.y_min  # Valeur contrainte par le rapport des dimensions
+
+    def init_ensemble(self):
+        self.ensemble = [False]*self.nb_points_y
+        for ny in range(self.nb_points_y):
+            self.ensemble[ny] = [False]*self.nb_points_x
+
+    def maj_zone_repr(self, nx_min, nx_max, ny_min):
+        tuple_zone_repr = (self.pix_to_x(nx_min), self.pix_to_x(nx_max), self.pix_to_y(ny_min))
+        self.init_zone_repr(tuple_zone_repr)
+
+    def get_ensemble(self):
+        """Méthode déterminant l'ensemble de Mandelbrot pour la zone de représentation courante
+
+        Attribue un booléen à tous les points de la zone selon que le point appartient
+        à l'ensemble de Mandelbrot ou non
+        """
+        # Parcours des points pour la zone courante
+        for ny in range(self.nb_points_y):
+            y = self.Kxy * ny + self.y_min  # ordonnée du point courant
+            for nx in range(self.nb_points_x):
+                x = self.Kxy * nx + self.x_min  # abscisse du point courant
+                # Premier test d'appartenance (à la cardioïde ou au bourgeon principal) 
+                p = sqrt((x-0.25)**2 + y**2)
+                if (x < p-2*p**2+0.25) or ((x+1)**2+y**2 < 1./16):
+                    self.ensemble[ny][nx] = True
+                # Deuxieme test d'appartenance
+                else :
+                    self.ensemble[ny][nx] = Mandelbrot.appartenance(x, y, self.n_iter)
+
+    @staticmethod
+    def appartenance(x, y, n_iter):
+        c = complex(x, y)
+        z = complex(0, 0)
+        appartient = False
+        n = 0
+        while n < n_iter and abs(z) <= 2:
+            z = z*z + c
+            n = n + 1
+        if n == n_iter:  # ou abs(z) <= 2
+            appartient = True
+        return appartient
+
+    def pix_to_x(self, px):
+        return self.Kxy * px + self.x_min
+
+    def pix_to_y(self, py):
+        return self.Kxy * py + self.y_min        
+
+
+#---------------------------------------- Callbacks ----------------------------------------#
+
+def clic(event):
+    "Callback destinée à définir le coin supérieur haut gauche du cadre de zoom"
+    global px_min, py_min, iD_cadre_zoom
+    px_min = event.x
+    py_min = event.y
+    iD_cadre_zoom = canevas.create_rectangle(px_min, py_min, px_min, py_min, outline='red')
+
+def cadre_zoom(event):
+    "Callback dessinant le cadre de zoom"
+    global px_max
+    px_max = event.x
+    taille = px_max - px_min
+    canevas.coords(iD_cadre_zoom, px_min, py_min, px_max, py_min+mandel.K*taille) # on contraint le cadre à respecter les proportions de la fenêtre
+
+def zoom(event):
+    "Callback effectuant un zoom sur la zone sélectionnée de la région actuelle"
+    # Modification du modèle
+    mandel.maj_zone_repr(px_min, px_max, py_min)
+    mandel.get_ensemble()
+    # Tracé
+    canevas.delete(ALL)
+    trace_ensemble(mandel)
+
+def trace_ensemble(mandel):
+    "Fonction de tracé effectif de l'ensemble de Mandelbrot"
+    for py in range(hauteur):
+        for px in range(largeur):
+            if mandel.ensemble[py][px] == True:
                 canevas.create_rectangle(px,py,px,py)
 
 
-def clic (event) :
-    "Fonction destinée à définir le coin supérieur haut gauche du cadre de zoom"
-    global ch_px, ch_py, iD_cadre_zoom
-    ch_px = event.x
-    ch_py = event.y
-    iD_cadre_zoom = canevas.create_rectangle(ch_px, ch_py, ch_px, ch_py, outline='red')
+#---------------------------------- Programme principal ----------------------------------#
 
-def cadre_zoom (event) :
-    "Fonction dessinant le cadre de zoom"
-    global cb_px, cb_py
-    cb_px = event.x
-    cb_py = event.y
-    canevas.coords(iD_cadre_zoom, ch_px, ch_py, cb_px, cb_py)
-
-def zoom (event) :
-    "Fonction effectuant un zoom sur la zone sélectionnée de la région actuellement représentée"
-    # On calcule les coordonnées du cadre à partir de la sélection effectuée
-    # puis on retrace l'ensemble dans la région créée
-    global min_x_courant, max_x_courant, min_y_courant, max_y_courant
-    canevas.delete(ALL)
-    Kx = (max_x_courant-min_x_courant)/largeur
-    Ky = (max_y_courant-min_y_courant)/hauteur
-    # Suite d'équations dans cet ordre pour ne pas mettre à jour min_x_courant et min_y_courant trop tôt...
-    max_x_courant = Kx * cb_px + min_x_courant
-    max_y_courant = Ky * cb_py + min_y_courant
-    min_x_courant = Kx * ch_px + min_x_courant
-    min_y_courant = Ky * ch_py + min_y_courant
-    trace_ensemble (min_x_courant, max_x_courant, min_y_courant, max_y_courant)
-
-
-############################# Programme principal #############################
+# Paramètres
+largeur, hauteur = 800, 800
+nb_points = (largeur, hauteur)
+zone_init = (-2., 1., -1.5) # (x_min, x_max, y_min), y_max est contraint par le rapport de longueur des axes
 
 # Création des éléments graphiques
 fenetre = Tk()
-largeur = 400
-hauteur = 400
-canevas = Canvas (fenetre, width=largeur, height=hauteur, bg="white")
+canevas = Canvas(fenetre, width=largeur, height=hauteur, bg="white")
 canevas.pack()
 
 # Définition des gestionnaires d'événements
@@ -82,13 +122,9 @@ canevas.bind("<Button-1>", clic)
 canevas.bind("<Button1-Motion>", cadre_zoom)
 canevas.bind("<Button1-ButtonRelease>", zoom)
 
-# Paramètres relatifs à la région représentée
-min_x_init, max_x_init = -2., 1.
-min_y_init, max_y_init = -1.5, 1.5
-min_x_courant, max_x_courant = min_x_init, max_x_init
-min_y_courant, max_y_courant = min_y_init, max_y_init
-
 # Tracé
-trace_ensemble (min_x_courant, max_x_courant, min_y_courant, max_y_courant)
+mandel = Mandelbrot(nb_points, zone_init)
+mandel.get_ensemble()
+trace_ensemble(mandel)
 
 fenetre.mainloop()
