a8e89fa1496f67ed3e779f898eb8b7f01ab36b7b
AlexandreLaurac <alexandre_laurac@yahoo.fr>
2025-01-31 23:59:40 +0100

    Amélioration des performances du calcul de l'ensemble par l'utilisation de calcul matriciel avec Numpy

--- ensemble_Mandelbrot.py
+++ ensemble_Mandelbrot.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import numpy as np
 from math import sqrt, copysign
 import sys, getopt
 import time
@@ -7,51 +8,60 @@ import time
 #---------------------------------------- Modèle ----------------------------------------#
 
 class Point():
-    """Classe des coordonnées x et y (flottantes) d'un point du plan"""
+    "Classe des coordonnées x et y (flottantes) d'un point du plan"
 
     def __init__(self, x=0, y=0):
         self.x = x
         self.y = y
 
 
-class DimPix():
-    """Classe des dimensions x et y (entières) d'une image en pixels"""
+class ImPix():
+    """Classe utilitaire de l'image en pixels sous-jacente à la zone de représentation.
+    
+    Un objet de cette classe possède :
+    - la hauteur et la largeur de l'image
+    - le rapport R de ces deux valeurs
+    - des matrices Numpy ligne [0:largeur-1] et colonne [0:hauteur-1] stockées et utilisées
+      lors du calcul de l'ensemble de Mandelbrot
+    """
 
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
+    def __init__(self, largeur, hauteur):
+        self.largeur = largeur
+        self.hauteur = hauteur
+        self.R = hauteur / largeur  # rapport des dimensions, pour l'image ET la zone de représentation
+        self.mat_px = np.linspace(0, largeur, num=largeur, endpoint=False)[np.newaxis]
+        self.mat_py = np.linspace(0, hauteur, num=hauteur, endpoint=False)[:,np.newaxis]
 
 
 class Zone():
     """Classe de données géométriques modélisant une zone de représentation
 
     Un objet de cette classe possède :
-    - un objet de type DimPix, contenant les dimensions en pixels de l'image de représentation sous-jacente
+    - un objet de type ImPix lié à la zone
     - deux points A et B correspondant au point haut gauche et au point bas droit de la zone (l'ordonnée
       du point B est contrainte pour respecter les dimensions de l'image)
     - des relations de passage entre les pixels et les points du plan grâce à trois coefficients et des méthodes
     """
 
-    def __init__(self, nb_points_x, nb_points_y, xa, xb, ya):
-        # Dimensions en pixels de la zone
-        self.dim_pix = DimPix(nb_points_x, nb_points_y)
-        self.K = nb_points_y / nb_points_x  # rapport des dimensions, pour l'image ET la zone de représentation
+    def __init__(self, nb_pixels_x, nb_pixels_y, xa, xb, ya):
+        # Image en pixels sous-jacente à la zone
+        self.im_pix = ImPix(nb_pixels_x, nb_pixels_y)
         # Bornes de la zone
         self.A = Point()  # point haut gauche
         self.B = Point()  # point bas droit
         self.init_bornes(xa, xb, ya)
 
     def init_bornes(self, xa, xb, ya):
-        self.Kxy = (xb - xa) / self.dim_pix.x  # Kxy == Kx == Ky
+        self.Kxy = (xb - xa) / self.im_pix.largeur  # Kxy == Kx == Ky
         # Point A
         self.A.x = xa
         self.A.y = ya
         # Point B
         self.B.x = xb
-        self.B.y = -self.K * (xb - xa) + ya  # Valeur contrainte par le rapport des dimensions (et signe - : voir plus bas)
+        self.B.y = -self.im_pix.R * (xb - xa) + ya  # Valeur contrainte par le rapport des dimensions (et signe - : voir plus bas)
 
     def maj_bornes_zoom(self, pxa, pxb, pya):
-        """Calcul de nouvelles bornes à partir de pixels de zoom"""
+        "Calcul de nouvelles bornes à partir de pixels de zoom"
         xa, xb, ya = self.pix_to_x(pxa), self.pix_to_x(pxb), self.pix_to_y(pya)
         self.init_bornes(xa, xb, ya)
 
@@ -61,7 +71,7 @@ class Zone():
         """
         Kx = (self.B.x - self.A.x) / (pxb - pxa)
         xa = self.A.x - Kx * pxa
-        xb = xa +  Kx * self.dim_pix.x
+        xb = xa +  Kx * self.im_pix.largeur
         Ky = (self.B.y - self.A.y) / (pyb - pya)
         ya = self.A.y - Ky * pya
         self.init_bornes(xa, xb, ya)
@@ -74,54 +84,33 @@ class Zone():
 
 
 class Mandelbrot():
-    """Classe modélisant l'ensemble de Mandelbrot
+    """Classe modélisant l'ensemble de Mandelbrot.
 
     L'ensemble est représenté sur une zone du plan et les points qui lui appartiennent
-    sont déterminés à partir du calcul d'une suite de récurrence avec au plus n_iter itérations
+    sont déterminés à partir du calcul d'une suite de récurrence avec n_iter itérations
     """
 
-    def __init__(self, nb_points_x, nb_points_y, xa, xb, ya, n_iter=100):
-        self.zone = Zone(nb_points_x, nb_points_y, xa, xb, ya)
+    def __init__(self, nb_pixels_x, nb_pixels_y, xa, xb, ya, n_iter=100):
+        self.zone = Zone(nb_pixels_x, nb_pixels_y, xa, xb, ya)
         self.n_iter = n_iter
-        self.init_ensemble()
-
-    def init_ensemble(self):
-        self.ensemble = [False]*self.zone.dim_pix.y
-        for ny in range(self.zone.dim_pix.y):
-            self.ensemble[ny] = [False]*self.zone.dim_pix.x
+        np.seterr(all='ignore')
 
     def calcul_ensemble(self):
-        """Méthode déterminant l'ensemble de Mandelbrot pour la zone de représentation courante
+        """Méthode déterminant l'ensemble de Mandelbrot pour la zone de représentation courante.
 
-        Attribue un booléen à tous les pixels de la zone selon que la relation de récurrence
-        zn+1 = zn * zn + c converge ou non. Le critère de convergence est le non-dépassement
-        de la valeur 2 en module après n_iter itérations
+        Attribue un booléen à tous les pixels de l'image selon que la relation de récurrence
+        zn+1 = zn * zn + c converge ou non (avec c = x + iy, x et y étant les coordonnées dans la
+        zone de représentation d'un pixel donné). Le critère de convergence est le non-dépassement
+        de la valeur 2 en module après n_iter itérations.
+
+        Les calculs sont réalisés matriciellement grâce à Numpy, une matrice contenant les valeurs
+        d'une grandeur donnée pour tous les pixels de l'image.
         """
-        # Parcours des points pour la zone courante
-        for ny in range(self.zone.dim_pix.y):
-            y = self.zone.pix_to_y(ny)  # ordonnée du point courant
-            for nx in range(self.zone.dim_pix.x):
-                x = self.zone.pix_to_x(nx)  # abscisse du point courant
-                # Premier test d'appartenance (à la cardioïde ou au bourgeon principal) 
-                p = sqrt((x-0.25)**2 + y**2)
-                if (x < p-2*p**2+0.25) or ((x+1)**2+y**2 < 1./16):
-                    self.ensemble[ny][nx] = True
-                # Deuxieme test d'appartenance
-                else :
-                    self.ensemble[ny][nx] = Mandelbrot.appartenance(x, y, self.n_iter)
-
-    @staticmethod
-    def appartenance(x, y, n_iter):
-        c = complex(x, y)
-        z = complex(0, 0)
-        appartient = False
-        n = 0
-        while n < n_iter and abs(z) <= 2:
+        c = self.zone.pix_to_x(self.zone.im_pix.mat_px) + 1j * self.zone.pix_to_y(self.zone.im_pix.mat_py)
+        z = np.zeros((self.zone.im_pix.hauteur, self.zone.im_pix.largeur), dtype=complex)
+        for n in range(self.n_iter):
             z = z*z + c
-            n = n + 1
-        if n == n_iter:  # ou abs(z) <= 2
-            appartient = True
-        return appartient
+        self.ensemble = np.abs(z) < 2
 
 
 #---------------------------------------- Vues ----------------------------------------#
